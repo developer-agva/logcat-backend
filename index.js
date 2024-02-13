b160315eaed7d2a9d5ed83f56885bc1bec006225
shivprakash121 <shivprakash@agvahealthtech.com>
2024-02-13 13:59:07 +0530

    E: GET - add hospitalAddress in user login API ||E: POST - Validate productCode in ventilator API

--- index.js
+++ index.js
@@ -173,8 +173,7 @@ const { messaging } = require("firebase-admin");
 const io = new Server(server, {
   cors: {
     // origin: "http://192.168.2.37:3000",
-    origin: "http://medtap.in" || "http://3.26.129.121:3000",
-    
+    origin: "http://medtap.in",
     methods: ["GET", "POST"],
   },
 });
